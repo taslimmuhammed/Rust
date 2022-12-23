d2495da01d1abc91f096dbf6bc5e9eecda688281
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-12-23 09:40:50 +0530

    vectors

---
+++ modules_crates/src/main.rs
@@ -0,0 +1,10 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
+fn main() {
+    eat_at_restaurant()
+}
---
+++ modules_crates/target/debug/deps/modules_crates-f029319f9b117887.d
@@ -0,0 +1,5 @@
+C:\Users\tasli\Desktop\Rust\Tutorial\modules_crates\target\debug\deps\modules_crates-f029319f9b117887.exe: src\main.rs
+
+C:\Users\tasli\Desktop\Rust\Tutorial\modules_crates\target\debug\deps\modules_crates-f029319f9b117887.d: src\main.rs
+
+src\main.rs:
