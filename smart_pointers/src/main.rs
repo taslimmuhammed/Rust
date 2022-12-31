9154c6e914bba2a286565aeedb3718dbb4a45fd6
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-12-31 16:14:49 +0530

    smart ptr -2

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc, cell::RefCell};
 struct MyBox<T>(T);
 
 impl<T> MyBox<T>{
@@ -7,12 +7,11 @@ impl<T> MyBox<T>{
     }
 }
 
-enum List{
-    Cons(i32, Box<List>),
-    Nil
-}
+// enum List{
+//     Cons(i32, Box<List>),
+//     Nil
+// }
 
-use List::{Cons,Nil};
 impl<T> Deref for MyBox<T>{
     type Target = T;
     
@@ -30,11 +29,64 @@ fn main() {
    let y = MyBox::new(5);
    assert_eq!(x,*(y.deref()));
    let c = String::from("testing");
-   drop(c); //callin the drop method on c
-
+   drop(c); //calling the drop method on c
+  // reference_counter()
+   refcell()
 }
 
-fn box_pointer(){
-   let mut list = Cons(1,Box::new(Cons(2,Box::new(Cons(3, Box::new(Nil))))));
-   let head = 
+
+fn reference_counter(){
+    enum List{
+        Cons(i32, Rc<List>),
+        Nil
+    }
+    use List::{Cons,Nil};
+
+    let a= Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3,Rc::new(Nil)))))));
+    let b= Cons(4, Rc::clone(&a));
+     //a.clone can also be used here
+    {
+        let c =Cons(5, Rc::clone(&a));
+        println!("No of pointers to a is :-{}",Rc::strong_count(&a));
+    }
+    //Rc keeps track of all the pointers pointing towards "a"
+    //And drops the value only when there are no pointers
+    println!("No of pointers to a is :-{}",Rc::strong_count(&a));
 }
+
+
+fn refcell(){
+
+    //Rc=>To have multiple pointers towards it
+    //RefCell=>To have mutable values
+    #[derive(Debug)]
+    enum List{
+        Cons(Rc<RefCell<i32>>, Rc<List>),
+        Nil
+    }
+    use List::{Cons,Nil};
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)),Rc::clone(&a));
+    let c =Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    
+
+    *value.borrow_mut() +=10;
+    // match b{
+    //     Cons(data, next_address) => {
+    //         match next_address::List {
+    //             Cons(data2, _) =>{},
+    //             Nil => {},
+    //         }
+    //     },
+    //     Nil => todo!(),
+    // }
+    println!("a after ={:?}",a);
+    println!("a after ={:?}",b);
+    println!("a after ={:?}",c);
+
+}  
+
